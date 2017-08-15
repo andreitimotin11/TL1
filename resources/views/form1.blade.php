fdd8a622ac9d5eebeb4eb68c8af12dd00cd43b2c
Andrei Timotin <andreitimotin11@gmail.com>
2017-08-15 18:32:00 +0300

    Am scos de pe proiect VerifyCsrfToken

---
+++ resources/views/form1.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="http://laravel1/comments" method="post">
+    <input type="text" name="name">
+    <textarea name="text"></textarea>
+    <input type="submit">
+    
+</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+Route::get('/page', function () {
+	echo '<pre>';
+	print_r($_ENV);
+	echo '</pre>';
+//	echo config('app.locale');
+	echo Config::set('app.locale', 'ru');
+	echo Config::get('app.locale');
+	echo env('APP_ENV');
+});
+Route::post('/comments', function () {
+	print_r($_POST);
+//	print_r($_GET);
+});
+Route::get('/form1', function (){
+	include __DIR__. "/../resources/views/form1.blade.php";
 });
